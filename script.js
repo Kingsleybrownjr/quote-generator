4d91f6270c72a16840e7339afa232afaa83ad8a0
kingsleybrownjr <kingsleybrownjr@yahoo.com>
2022-05-10 18:36:49 -0500

    added loader to the project, project completed

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
 // Display new quote
 const newQuote = () => {
+	showLoader();
 	const randomNumber = Math.floor(Math.random() * apiQuotes.length);
 	// Pick random quote from apiQuotes array
 	const quote = apiQuotes[randomNumber];
@@ -18,7 +20,10 @@ const newQuote = () => {
 	} else {
 		quoteText.classList.remove('long-quote');
 	}
+
+	// Set quote, hide loader
 	quoteText.textContent = quote.text;
+	hideLoader();
 
 	// Check if author field is empty and replace with "Unknown"
 	if (!quote.author) {
@@ -30,6 +35,7 @@ const newQuote = () => {
 
 // Get quotes from API
 const getQuotes = async () => {
+	showLoader();
 	const apiUrl = 'https://type.fit/api/quotes';
 
 	try {
@@ -46,6 +52,18 @@ const tweetQuote = () => {
 	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
 
 	window.open(twitterUrl, '_blank');
+};
+
+// Show Loader
+const showLoader = () => {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+};
+
+// Hide Loader
+const hideLoader = () => {
+	loader.hidden = true;
+	quoteContainer.hidden = false;
 };
 
 twitterBtn.addEventListener('click', tweetQuote);
